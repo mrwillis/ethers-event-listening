4b7b993e68fe42c2241fb30204f0476b821c93e2
Julian Wilson <julian@nextgenbt.com>
2018-09-19 11:32:28 -0400

    Update expect statement

--- test/event_contract.js
+++ test/event_contract.js
@@ -6,7 +6,7 @@ const eventContractAbi = require("../build/contracts/EventContract.json")
 contract("EventContract", accounts => {
     const ownerSigner = provider.getSigner(accounts[0]);
     const eventContract = new Contract(EventContract.address, eventContractAbi.abi, ownerSigner)
-    it("should work", async () => {
+    it("should fire an event and listen to it", async () => {
         eventContract.ontestevent = function(a, b) {
             console.log(a)
             console.log(b)
@@ -21,6 +21,7 @@ contract("EventContract", accounts => {
             console.log(b);
         })
         await eventContract.emitEvent(utils.bigNumberify(100));
-        expect(true).to.be.true
+        const newNumber = await eventContract.testNumber();
+        expect(newNumber.eq(utils.bigNumberify(100))).to.be.true;
     })
 }) 
