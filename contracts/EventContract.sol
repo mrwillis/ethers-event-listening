50ffe56c76db6aa64c35c1fbaaa54fe98f895ce7
Julian Wilson <julian@nextgenbt.com>
2018-09-19 12:51:55 -0400

    Add truffle and another try.

--- contracts/EventContract.sol
+++ contracts/EventContract.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.24;
-pragma experimental ABIEncoderV2;
 pragma experimental "v0.5.0";
+pragma experimental ABIEncoderV2;
 
 
 contract EventContract {
--- test/event_contract.js
+++ test/event_contract.js
@@ -1,15 +1,17 @@
 const {providers, Contract, utils} = require("ethers")
 const provider = new providers.Web3Provider(web3.currentProvider);
+const jsonRpcProvider = new providers.JsonRpcProvider("http://127.0.0.1:9545");
 const EventContract = artifacts.require("EventContract");
 const eventContractAbi = require("../build/contracts/EventContract.json")
 
 contract("EventContract", accounts => {
-    const ownerSigner = provider.getSigner(accounts[0]);
+    const ownerSigner = jsonRpcProvider.getSigner(accounts[0]);
     const eventContract = new Contract(EventContract.address, eventContractAbi.abi, ownerSigner)
     it("should fire an event and listen to it", async () => {
-        eventContract.ontestevent = function(a, b) {
-            console.log(a)
-            console.log(b)
+        eventContract.ontestevent = function(a, b, c) {
+            console.log(a);
+            console.log(b);
+            console.log(c);
         }
         const allFilter = eventContract.filters.TestEvent(null, null);
         eventContract.on(allFilter, (a, b) => {
@@ -17,6 +19,10 @@ contract("EventContract", accounts => {
             console.log(b);
         })
         eventContract.addListener("TestEvent", (a, b) => {
+            console.log(a);
+            console.log(b);
+        })
+        eventContract.on('TestEvent', function (a,b) {
             console.log(a);
             console.log(b);
         })
